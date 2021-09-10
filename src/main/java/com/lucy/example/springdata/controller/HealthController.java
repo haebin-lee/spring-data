23d8654f8ce4e9cef25e166e4071ed97d1c7b145
이해빈 <luckylucy@encar.com>
2021-09-10 13:53:33 +0900

    spring initialize

---
+++ src/main/java/com/lucy/example/springdata/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.lucy.example.springdata.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Success";
+    }
+}
